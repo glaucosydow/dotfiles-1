f0b491eb643a7bd3e79636377b03535c2f35b2f0
Rafael Rinaldi <rafael.rinaldi@gmail.com>
2013-07-18 23:51:41 -0300

    Adding Soda theme to the pipeline.

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -1,13 +1,20 @@
 user=`whoami`
-path="/Users/$user/Library/Application Support/Sublime Text 2/Packages/User"
-snippets="$path/st-snippets"
+path="/Users/$user/Library/Application Support/Sublime Text 2"
+snippets="$path/Packages/User/st-snippets"
+soda="$path/Packages/Theme - Soda"
 
 cp -r ./*sublime* "$path"
 
 echo "Installing snippets..."
 
 if [[ ! -d $snippets ]]; then
-	cd "$path" && git clone git@github.com:rafaelrinaldi/st-snippets.git
+	cd "$path/Packages/User" && git clone git@github.com:rafaelrinaldi/st-snippets.git
 else
 	cd "$snippets" && git pull
+fi
+
+echo "Installing Soda theme..."
+
+if [[ ! -d $soda  ]]; then
+	cd "$path/Packages" && git clone git@github.com:buymeasoda/soda-theme.git "Theme - Soda"
 fi
